d51882fe4bdc5ee499440c357e79112905493db2
Aashish0190 <ashishkumar44521@gmail.com>
2024-09-17 02:25:02 +0530

    create api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose  from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js'
 dotenv.config();
 
 
@@ -13,13 +14,13 @@ mongoose.connect(process.env.MONGO).then(() =>{
     console.log(error);
 })
 
-
-
-
-
-
 const app = express();
+
+
 
 app.listen(3000, () =>{
     console.log("Sever is running on port 3000");
 })
+
+
+app.use("/api/user",userRouter);
